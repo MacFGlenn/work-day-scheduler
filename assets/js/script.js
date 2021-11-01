1e613f8470fb3c695d31bd8cca52328f319d5cfa
MFG <macfglenn@gmail.com>
2021-10-31 20:14:49 -0400

    added time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 var currentDate = moment();
-// var leadCl = $('lead');
 
-document.getElementById('current-day').textContent = currentDate.format('dddd MMMM Do');
+document.getElementById("current-day").textContent =
+  currentDate.format("dddd MMMM Do");
 
-console.log(leadCl);
-console.log(currentDate.calendar());
